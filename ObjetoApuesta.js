d97dff26d9d78dc05a8f9d194c99dc540402b937
Enrique Muñoz <enriquemunozromera@outlook.es>
2020-10-26 12:24:29 +0100

    Objeto Apuesta + POST

---
+++ ObjetoApuesta.js
@@ -0,0 +1,6 @@
+﻿var Apuesta = function (idApuesta, usuario, resultado, fecha) {
+    this.idApuesta = idApuesta;
+    this.usuario = usuario;
+    this.resultado = resultado;
+    this.fecha = fecha;
+}
